d80f877ca07bc770bbd38fe8cd7a10277d78acb5
Lance Shi <Lanceyshi@gmail.com>
2018-01-30 22:32:59 +1000

    inital commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/conFusion';
+
+MongoClient.connect(url, (err, db) => {
+    assert.equal(err, null);
+
+    console.log('Connected correctly to server');
+
+    const conFusionDb = db.db("conFusion");
+    const collection = conFusionDb.collection("dishes");
+    collection.insertOne({"name": "Uthappizza", "description": "test"}, 
+    (err, result) => {
+        assert.equal(err, null);
+
+        console.log("After Insert:\n");
+        console.log(result.ops);
+
+        collection.find({}).toArray((err, docs) => {
+            assert.equal(err, null);
+
+            console.log("Found:\n");
+            console.log(docs);
+
+            conFusionDb.dropCollection("dishes", (err, result) => {
+                assert.equal(err, null);
+
+                db.close();
+            })
+        });
+    });
+});
